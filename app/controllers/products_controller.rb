31205e58bbaef3318495195e31c40ab6a1727e7d
Safiy Cham <csafi13@yahoo.com>
2017-11-17 09:57:21 -0800

    create new product form page with submission functionality.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,29 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all?
+    @products = Product.all
     render :index
   end
 
   def show
     @product = Product.find(params[:id])
     render :show
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_origin)
   end
 end
