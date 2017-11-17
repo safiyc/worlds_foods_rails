f01bc59e5d92d2a993905bca63cc79da8a5617b2
Safiy Cham <csafi13@yahoo.com>
2017-11-17 10:59:59 -0800

    add update functionality to existing product entries.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,4 +26,23 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :cost, :country_origin)
   end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :cost, :country_origin)
+    end
 end
