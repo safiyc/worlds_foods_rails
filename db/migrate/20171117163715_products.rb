caf3489124a5300d09aceabd848c88ca8eff4973
Safiy Cham <csafi13@yahoo.com>
2017-11-17 08:48:23 -0800

    create initial rails project files, bundle install gems and set models for products and reviews.

---
+++ db/migrate/20171117163715_products.rb
@@ -0,0 +1,11 @@
+class Products < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.column :name, :string
+      t.column :cost, :string
+      t.column :country_origin, :string
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20171117163744_reviews.rb
@@ -0,0 +1,11 @@
+class Reviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.column :author, :string
+      t.column :content_body, :string
+      t.column :rating, :integer
+
+      t.timestamps
+    end
+  end
+end
