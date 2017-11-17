b7ab279bbe31562b487b2655d9aa31f4642dc7e6
Safiy Cham <csafi13@yahoo.com>
2017-11-17 09:18:35 -0800

    create controller and view for show page of individual product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     @products = Product.all?
     render :index
   end
+
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
 end
