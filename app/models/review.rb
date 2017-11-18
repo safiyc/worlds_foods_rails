229d8420d70535d62044051118aa4086893bebb0
Safiy Cham <csafi13@yahoo.com>
2017-11-17 20:30:50 -0800

    add review validations for rating integer from 1 to 5, and content_body character length 50 to 250.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   validates :author, :content_body, :rating, :presence => true
+
+  validates_format_of :rating, :with => /[1-5]/, :on => :create
+  validates_length_of :rating, :maximum => 1
+
 end
