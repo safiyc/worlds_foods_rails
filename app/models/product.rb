88792f36757eaa839a21d10b4c318b249318f597
Safiy Cham <csafi13@yahoo.com>
2017-11-23 14:21:27 -0800

    add faker gem and seeds database, and minor styling changes.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :name, :cost, :country_origin, :presence => true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 Product.destroy_all
 Review.destroy_all
 
-5.times do
-  Product.create!(name: Faker::Food.ingredient, cost: Faker::Commerce.price, country_origin: Faker::Address.country)
-end
+50.times do |index|
+  product = Product.create!(name: Faker::Food.ingredient, cost: Faker::Commerce.price, country_origin: Faker::Address.country)
 
-# 20.times do
-#   Review.create!(author: Faker::Name.name, content_body: Faker::Lorem.characters(150), rating: Faker::Number.between(1, 5), product_id: Faker::Number.number(1))
-# end
+  5.times do |index|
+    Review.create!(author: Faker::Name.name, content_body: Faker::Lorem.paragraph(3, true, 2), rating: Faker::Number.between(1, 5), product_id: product.id)
+  end
+end
